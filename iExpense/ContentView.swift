391da02d54f256ba6b8aa04f4f4865d45ab64f82
Michele Volpato <mvolpato@users.noreply.github.com>
2020-12-14 22:41:06 +0100

    Add user default read and write

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -12,57 +12,26 @@ class User: ObservableObject {
     @Published var lastName = "Baggins"
 }
 
-struct SecondView: View {
-    @Environment(\.presentationMode) var presentationMode
-
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
-
-    var name: String
-
-    var body: some View {
-        NavigationView {
-            VStack {
-                Text("Hello, \(name)!")
-                List {
-                    ForEach(numbers, id: \.self) {
-                        Text("\($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-
-                Button("Add Number") {
-                    self.numbers.append(self.currentNumber)
-                    self.currentNumber += 1
-                }
-                Button("Dismiss") {
-                    self.presentationMode.wrappedValue.dismiss()
-                }
-            }
-            .navigationBarItems(leading: EditButton())
-        }
-    }
-
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
-    }
-}
-
 struct ContentView: View {
     @ObservedObject private var user = User()
     @State private var showingSheet = false
-
+    
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
-            Button("Show Sheet") {
-                self.showingSheet.toggle()
-            }
-            .sheet(isPresented: $showingSheet) {
-                SecondView(name: "@twostraws")
+        NavigationView {
+            VStack {
+                Text("Your name is \(user.firstName) \(user.lastName).")
+                
+                TextField("First name", text: $user.firstName)
+                TextField("Last name", text: $user.lastName)
+                Button("Show Sheet") {
+                    self.showingSheet.toggle()
+                }
+                NavigationLink(destination: StoreDataView()) {
+                    Text("Check stored count")
+                }
+                .sheet(isPresented: $showingSheet) {
+                    SecondView(name: "@twostraws")
+                }
             }
         }
     }
