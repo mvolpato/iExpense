e3edb64a847c3fb65c016757309adf09caa9ce94
Michele Volpato <mvolpato@users.noreply.github.com>
2020-12-14 22:19:04 +0100

    Get class working as a state

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Michele Volpato on 14/12/2020.
+//
+
+import SwiftUI
+
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
+struct ContentView: View {
+    @ObservedObject private var user = User()
+
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName).")
+
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Michele Volpato on 14/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
