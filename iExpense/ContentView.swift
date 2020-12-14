0c91a3b75bc8c657af4d34f2450816a7a9e720e8
Michele Volpato <mvolpato@users.noreply.github.com>
2020-12-14 22:31:32 +0100

    onDelete and EditButton

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -12,8 +12,45 @@ class User: ObservableObject {
     @Published var lastName = "Baggins"
 }
 
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+
+    var name: String
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Hello, \(name)!")
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("\($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+
+                Button("Add Number") {
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                }
+                Button("Dismiss") {
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+            }
+            .navigationBarItems(leading: EditButton())
+        }
+    }
+
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+}
+
 struct ContentView: View {
     @ObservedObject private var user = User()
+    @State private var showingSheet = false
 
     var body: some View {
         VStack {
@@ -21,6 +58,12 @@ struct ContentView: View {
 
             TextField("First name", text: $user.firstName)
             TextField("Last name", text: $user.lastName)
+            Button("Show Sheet") {
+                self.showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                SecondView(name: "@twostraws")
+            }
         }
     }
 }
