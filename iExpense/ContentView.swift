6d72b86ac24ead9928f7a19162aa23f574c09ae2
Michele Volpato <mvolpato@users.noreply.github.com>
2020-12-15 21:21:08 +0100

    Day 37

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,33 +7,81 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
+struct ExpenseItem: Identifiable, Codable {
+    let id: UUID
+    let name: String
+    let type: String
+    let amount: Int
+
+    init(id: UUID = UUID(), name: String, type: String, amount: Int) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.amount = amount
+    }
+
+}
+
+class Expenses: ObservableObject {
+    @Published var items: [ExpenseItem] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+
+        self.items = []
+    }
 }
 
 struct ContentView: View {
-    @ObservedObject private var user = User()
-    @State private var showingSheet = false
+    @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Your name is \(user.firstName) \(user.lastName).")
-                
-                TextField("First name", text: $user.firstName)
-                TextField("Last name", text: $user.lastName)
-                Button("Show Sheet") {
-                    self.showingSheet.toggle()
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+
+                        Spacer()
+                        Text("$\(item.amount)")
+                    }
                 }
-                NavigationLink(destination: StoreDataView()) {
-                    Text("Check stored count")
-                }
-                .sheet(isPresented: $showingSheet) {
-                    SecondView(name: "@twostraws")
-                }
+                .onDelete(perform: removeItems)
             }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        self.showingAddExpense = true
+                                    }) {
+                                        Image(systemName: "plus")
+                                    }
+            )
         }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: self.expenses)
+        }
+    }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
